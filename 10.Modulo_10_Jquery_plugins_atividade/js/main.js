b0a7933e7142270df9c443c3a2485d5772bab786
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-08-22 21:39:51 -0300

    exercicio modulo jquery plugins

---
+++ 10.Modulo_10_Jquery_plugins_atividade/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#carousel-images').slick({
+        autoplay: true,
+    });
+
+    $('.menu-hamburger').click(function() {
+        $('nav').slideToggle()
+    });
+
+    $('#cpf').mask('000.000.000-00');
+    $('#telefone').mask('(00) 00000-0000');
+    $('#cep').mask('00000-000');
+});
