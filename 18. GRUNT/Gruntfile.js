94db068cff6c8419b9f40e2654cff2b8a453a007
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-09-12 16:43:53 -0300

    grunt

--- 18. GRUNT/Gruntfile.js
+++ 18. GRUNT/Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
         less: {
             development: {
                 files: {
-                    'main.css': 'main.less'
+                    'dev/styles/main.css': 'src/styles/main.less'
                 }
             },
             production: {
@@ -12,36 +12,74 @@ module.exports = function(grunt) {
                     compress: true,
                 },
                 files: {
-                    'main.min.css' : 'main.less'
+                    'dist/styles/main.min.css' : 'src/styles/main.less'
                 }
             }
         },
-        sass: {
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development']
+            }
+        },
+        replace: {
+            dev: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
             dist: {
                 options: {
-                    style: 'compressed'
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.min.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
                 },
                 files: {
-                    'main2.css' : 'main.scss'
+                    'prebuild/index.html' : 'src/index.html'
                 }
             }
         },
-        concurrent: {
-            target: ['olaGrunt', 'less', 'sass']
-        }
-    })
-
-    grunt.registerTask("olaGrunt", function() {
-        const done = this.async();
-        setTimeout(function() {
-            console.log("Olá, grunt!");
-            done();
-        }, 3000);
+        clean: ['prebuild']
     })
 
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask('default', ['concurrent']);
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist']);
 }
