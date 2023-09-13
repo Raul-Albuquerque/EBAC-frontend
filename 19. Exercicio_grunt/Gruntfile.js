97c8b77364d65745dbcc3b98e4ab42e83e67f8ff
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-09-12 21:50:37 -0300

    exercicio_grunt

---
+++ 19. Exercicio_grunt/Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css' : 'src/styles/main.less'
+                }
+            }
+        },
+        uglify: {
+            target: {
+                files: {
+                    'dev/scripts/main.js' : 'src/scripts/main.js'
+                }
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['less:development', 'uglify']);
+}
