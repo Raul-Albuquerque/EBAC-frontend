658ced22db9c4c5c272204fc41472b3872cc7f10
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-10-12 18:31:49 -0300

    oo ES6

---
+++ 25. ES6+/src/oo.js
@@ -0,0 +1,36 @@
+class Pokemon {
+    #hp = 100;
+
+    constructor(nomeDoPokemon, tipoDoPokemon) {
+        this.nome = nomeDoPokemon;
+        this.tipo = tipoDoPokemon;
+    }
+
+    atacar(nomeDoAtaque) {
+        console.log(`${this.nome} atacou com ${nomeDoAtaque}`);
+    }
+
+    recebeuAtaque() {
+        this.#hp -= 10;
+    }
+
+    exibeHp() {
+        console.log(this.#hp);
+    }
+}
+
+class Pikachu extends Pokemon {
+    constructor() {
+        super('Pikachu', 'Elétrico')
+    }
+
+    atacar() {
+        console.log(`${this.nome} atacou com choque do trovão`);
+    }
+}
+
+const pikachuDoAsh = new Pikachu();
+pikachuDoAsh.recebeuAtaque();
+pikachuDoAsh.atacar();
+pikachuDoAsh.exibeHp();
+console.log(pikachuDoAsh.hp);
