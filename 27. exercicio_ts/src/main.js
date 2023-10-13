3777cbf67d7996240bd7349720f55e7ec665585e
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-10-13 18:32:19 -0300

    exercicio_ts

---
+++ 27. exercicio_ts/src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(numberA, numberB) {
+    return numberA * numberB;
+}
+console.log(multiplicacao(10, 20));
+function saldar(nome) {
+    return `Olá ${nome}`;
+}
+console.log(saldar("Raul"));
---
+++ 27. exercicio_ts/src/main.ts
@@ -0,0 +1,7 @@
+function multiplicacao(numberA: number, numberB: number) :number {
+    return numberA * numberB;
+}
+
+function saldar(nome: string) :string {
+    return `Olá ${nome}`;
+}
