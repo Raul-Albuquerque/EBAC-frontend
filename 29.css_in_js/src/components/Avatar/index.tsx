b0b607919f2c41b6b3626a6250fca5798df563d7
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-10-23 21:01:58 -0300

    Estilizando a sidebar

---
+++ 29.css_in_js/src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfl } from './styles'
+
+const Avatar = () => <FotoPerfl src="https://github.com/Raul-Albuquerque.png" />
+
+export default Avatar
--- 29.css_in_js/src/containers/Sidebar/index.tsx
+++ 29.css_in_js/src/containers/Sidebar/index.tsx
@@ -1,9 +1,19 @@
 import Titulo from '../../components/Titulo'
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Raul-Albuquerque.png" />
-    <Titulo fontSize={20}>Raul Albuquerque</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Raul Albuquerque</Titulo>
+      <Paragrafo tipo="secundario">Raul-Albuquerque</Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Front End
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
